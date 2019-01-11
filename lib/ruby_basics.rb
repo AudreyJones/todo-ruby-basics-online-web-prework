793f7f4b0411a5a3885039948ef1494547d91596
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-11 18:11:50 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -18,11 +18,11 @@ def greeting(greeting,name)
 
 end
 
-def return_a_value
+def return_a_value(phrase)
   return phrase
 end
 
-def last_evaluated_value
+def last_evaluated_value(phrase)
   return phrase
 end
 
